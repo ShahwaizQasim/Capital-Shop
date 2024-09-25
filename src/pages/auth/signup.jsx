a96c4ef27c20e0286dae9c8fc60049ab47acc107
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2024-09-25 12:43:38 -0700

    useState use

--- src/pages/auth/signup.jsx
+++ src/pages/auth/signup.jsx
@@ -1,13 +1,10 @@
-import { useForm } from "react-hook-form"
+import { useForm } from "react-hook-form";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../utils/firebase";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
-
-
 function SignUp() {
-
   const {
     register,
     handleSubmit,
@@ -16,81 +13,104 @@ function SignUp() {
     formState: { errors },
   } = useForm();
 
-
   const [UserName, setUserName] = useState("");
   const [Email, setEmail] = useState("");
   const [Password, setPassword] = useState("");
 
-  const onSubmit = () => {console.log('data')};
+  const onSubmit = () => {
+    
+  };
 
   return (
     <>
-    <div className="container-fluid bg-success">
-      <div className="container">
-        <div className="row">
-          <h1
-            className="text-center text-light mt-5 fw-bold d-inline"
-            style={{ fontFamily: '"Courier New", Courier, monospace' }}
-          >
-            Shah Store
-          </h1>
-          <div className="col-10 mt-4 m-auto pb-5 box">
-            <h1 className="text-dark fw-bold heading">Sign Up</h1>
-            <div className="row">
-              <div className="col-lg-6 col-md-6 col-sm-12">
-                <form onSubmit={handleSubmit(onSubmit)}>
-                  <input
-                    type="text"
-                    placeholder="Full Name"
-                    {...register("UserName", { required: true, maxLength: 15 })}
-                    className="form-control mt-4 mb-2"
-                    autoComplete="on"
-                  />
-                  {errors.UserName && (<span className="error_msg">This field is required</span>)}
+      <div className="container-fluid bg-success">
+        <div className="container">
+          <div className="row">
+            <h1
+              className="text-center text-light mt-5 fw-bold d-inline"
+              style={{ fontFamily: '"Courier New", Courier, monospace' }}
+            >
+              Shah Store
+            </h1>
+            <div className="col-10 mt-4 m-auto pb-5 box">
+              <h1 className="text-dark fw-bold heading">Sign Up</h1>
+              <div className="row">
+                <div className="col-lg-6 col-md-6 col-sm-12">
+                  <form onSubmit={handleSubmit(onSubmit)}>
+                    <input
+                      type="text"
+                      placeholder="Full Name"
+                      value={UserName}
+                      onChange={(e) => setUserName(e.target.value)}
+                      {...register("UserName", {
+                        required: true,
+                        maxLength: 25,
+                      })}
+                      className="form-control mt-4 mb-2"
+                      autoComplete="on"
+                    />
+                    {errors.UserName && (
+                      <span className="error_msg">This field is required</span>
+                    )}
 
-                  <input
-                    type="email"
-                    placeholder="Email"
-                    {...register("email", { required: true, maxLength: 15 })}
-                    className="form-control mt-4"
-                  />
-                  {errors.email && (<span className="error_msg">This field is required</span>)}
+                    <input
+                      type="email"
+                      placeholder="Email"
+                      value={Email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      {...register("email", { required: true, maxLength: 25 })}
+                      className="form-control mt-4"
+                    />
+                    {errors.email && (
+                      <span className="error_msg">This field is required</span>
+                    )}
 
-                  <input
-                    type="password"
-                    placeholder="Password"
-                    {...register("Password", { required: true, maxLength: 15 })}
-                    className="form-control mt-4 mb-2"
-                    maxLength={15}
-                  />
-                  {errors.Password && (<span className="error_msg">This field is required</span>)}
+                    <input
+                      type="password"
+                      placeholder="Password"
+                      value={Password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      {...register("Password", {
+                        required: true,
+                        maxLength: 15,
+                      })}
+                      className="form-control mt-4 mb-2"
+                    />
+                    {errors.Password && (
+                      <span className="error_msg">
+                        Must at least 10 characters
+                      </span>
+                    )}
 
-                  <div className="message error_message" />
-                  <span className="ps-2 para">Already have an Account ?</span>
-                  <Link to="/SignIn" className="ps-1 para">
-                    Log in
-                  </Link>
-                  <center>
-                    <a
-                      href="../pages/login.html"
-                      style={{
-                        textDecoration: "none",
-                        color: "aliceblue",
-                        fontWeight: 600,
-                      }}
-                    >
-                      <button className="btn btn-dark w-50 mt-3 mb-3 button" type="submit">
-                        SignUp
-                      </button>
-                    </a>
-                  </center>
-                </form>
+                    <div className="message error_message" />
+                    <span className="ps-2 para">Already have an Account ?</span>
+                    <Link to="/SignIn" className="ps-1 para">
+                      Log in
+                    </Link>
+                    <center>
+                      <Link
+                        to="/SignIn"
+                        style={{
+                          textDecoration: "none",
+                          color: "aliceblue",
+                          fontWeight: 600,
+                        }}
+                      >
+                        <button
+                          className="btn btn-dark w-50 mt-3 mb-3 button"
+                          type="submit"
+                        >
+                          SignUp
+                        </button>
+                      </Link>
+                    </center>
+                  </form>
+                </div>
+                <div className="col-lg-6 col-md-6 col-sm-12" />
               </div>
-              <div className="col-lg-6 col-md-6 col-sm-12" />
             </div>
           </div>
         </div>
-      </div>
       </div>
     </>
   );
